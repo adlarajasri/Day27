64233fb91c5f37bdbcf3de9bd6137aa1c1dd5e0c
adlarajasri <129931618+adlarajasri@users.noreply.github.com>
2023-05-10 15:20:03 +0530

    Add files via upload

---
+++ MyFirst.CoreWeb/MyFirst.CoreWeb/Pages/My.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyFirst.CoreWeb.Pages
+{
+    public class MyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
