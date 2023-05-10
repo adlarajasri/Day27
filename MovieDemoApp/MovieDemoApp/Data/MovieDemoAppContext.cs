35bf6ade7ef44a1db72c04b39d826d839294b193
adlarajasri <129931618+adlarajasri@users.noreply.github.com>
2023-05-10 15:12:04 +0530

    Add files via upload

---
+++ MovieDemoApp/MovieDemoApp/Data/MovieDemoAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MovieDemoApp.Models;
+
+namespace MovieDemoApp.Data
+{
+    public class MovieDemoAppContext : DbContext
+    {
+        public MovieDemoAppContext (DbContextOptions<MovieDemoAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MovieDemoApp.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ MovieDemoApp/MovieDemoApp/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MovieDemoApp.Data;
+using MovieDemoApp.Models;
+
+namespace MovieDemoApp.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MovieDemoApp.Data.MovieDemoAppContext _context;
+
+        public IndexModel(MovieDemoApp.Data.MovieDemoAppContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Movie != null)
+            {
+                Movie = await _context.Movie.ToListAsync();
+            }
+        }
+    }
+}
